11e367b7ff2ae02c5b8d3ff3a52e456068fc1e02
tiemvanderdeure <tvd@sund.ku.dk>
2023-11-28 11:47:24 +0100

    fixed package references

--- src/SpeciesDistributionModels.jl
+++ src/SpeciesDistributionModels.jl
@@ -1,14 +1,16 @@
 module SpeciesDistributionModels
 
-import MLJ, GLM, CategoricalArrays, Tables, StatsBase, PrettyTables, Rasters, EvoTrees, DecisionTree
+using MLJ 
+
+import GLM, Tables, StatsBase, PrettyTables, Rasters, EvoTrees, DecisionTree
+
+import CategoricalArrays.CategoricalArray
 
 export SDMensemble, predict, sdm, select, machines, machine_keys
 
 include("models.jl")
 include("ensemble.jl")
 include("predict.jl")
-
-
 
 end
 
