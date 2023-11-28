daa8a3348bbd14eb618fe39516a69044d960f149
tiemvanderdeure <tvd@sund.ku.dk>
2023-11-28 15:53:11 +0100

    add Makie as dependency, fix function references

--- src/SpeciesDistributionModels.jl
+++ src/SpeciesDistributionModels.jl
@@ -1,16 +1,18 @@
 module SpeciesDistributionModels
 
-using MLJ
+using MLJ 
 
 import GLM, Tables, StatsBase, PrettyTables, Rasters, EvoTrees, DecisionTree, Makie, Shapley
 
-using Makie: Toggle, Label, lines!, Axis, Figure, GridLayout, lift, Shapley
+using Makie: Toggle, Label, lines!, Axis, Figure, GridLayout, lift, Shapley, Makie
 
 using Rasters: Raster, RasterStack
 
+using Makie: Toggle, Label, lines!, Axis, Figure, GridLayout, lift
+
 import CategoricalArrays.CategoricalArray
 
-export SDMensemble, predict, sdm, select, machines, machine_keys, interactive_evaluation, shap
+export SDMensemble, predict, sdm, select, machines, machine_keys, shap, interactive_evaluation
 
 include("models.jl")
 include("ensemble.jl")
