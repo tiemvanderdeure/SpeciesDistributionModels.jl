22b8271eb850203af42b7863a29635496f9c0618
tiemvanderdeure <tvd@sund.ku.dk>
2023-11-29 12:42:01 +0100

    load models silently

--- src/models.jl
+++ src/models.jl
@@ -1,8 +1,8 @@
 # Load in models and wrap them to hide @load
 
-const lbc = MLJ.@load LinearBinaryClassifier pkg=GLM
-const etc = MLJ.@load EvoTreeClassifier pkg=EvoTrees
-const rf = MLJ.@load RandomForestClassifier pkg=DecisionTree
+const lbc = MLJ.@load LinearBinaryClassifier pkg=GLM verbosity = 0
+const etc = MLJ.@load EvoTreeClassifier pkg=EvoTrees verbosity = 0
+const rf = MLJ.@load RandomForestClassifier pkg=DecisionTree verbosity = 0
 
 linear_model(; kw...) = lbc(; kw...)
 boosted_regression_tree(; kw...) = etc(; kw...)
