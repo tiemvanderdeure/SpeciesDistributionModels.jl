73fb8f86aa8f3621563240c712b3da075d226ec2
tiemvanderdeure <tvd@sund.ku.dk>
2023-11-29 12:40:08 +0100

    fixed references to functions in Rasters

--- src/SpeciesDistributionModels.jl
+++ src/SpeciesDistributionModels.jl
@@ -4,6 +4,8 @@ using MLJ
 
 import GLM, Tables, StatsBase, PrettyTables, Rasters, EvoTrees, DecisionTree
 
+using Rasters: Raster, RasterStack
+
 import CategoricalArrays.CategoricalArray
 
 export SDMensemble, predict, sdm, select, machines, machine_keys
