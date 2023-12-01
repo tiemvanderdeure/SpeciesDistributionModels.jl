61ce662dd005dae704ffe44440284f656b533a9a
tiemvanderdeure <tvd@sund.ku.dk>
2023-12-01 12:27:20 +0100

    add shapley values

--- src/SpeciesDistributionModels.jl
+++ src/SpeciesDistributionModels.jl
@@ -2,19 +2,20 @@ module SpeciesDistributionModels
 
 using MLJ
 
-import GLM, Tables, StatsBase, PrettyTables, Rasters, EvoTrees, DecisionTree, Makie
+import GLM, Tables, StatsBase, PrettyTables, Rasters, EvoTrees, DecisionTree, Makie, Shapley
 
-using Makie: Toggle, Label, lines!, Axis, Figure, GridLayout, lift
+using Makie: Toggle, Label, lines!, Axis, Figure, GridLayout, lift, Shapley
 
 using Rasters: Raster, RasterStack
 
 import CategoricalArrays.CategoricalArray
 
-export SDMensemble, predict, sdm, select, machines, machine_keys, interactive_evaluation
+export SDMensemble, predict, sdm, select, machines, machine_keys, interactive_evaluation, shap
 
 include("models.jl")
 include("ensemble.jl")
 include("predict.jl")
+include("explain.jl")
 include("plots.jl")
 
 end
