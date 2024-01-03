0049300cd0a2338d52c19a71c6c0dd226fe520c2
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-01-03 16:58:04 +0100

    draft interface

---
+++ src/interface.jl
@@ -0,0 +1,41 @@
+"""
+
+"""
+
+function sdm_ensemble end
+
+
+"""
+    evaluate(ensemble::SDMensemble; measures)
+
+Description goes here
+
+## Arguments
+- `ensemble`: of type `SDMensemble` that is to be evaluated
+
+## Keywords
+- `measures`: measures to be evaluated
+
+## Returns
+
+"""
+
+function evaluate end
+
+
+"""
+    response_curves(ensemble::SDMensemble, method = Shapley())
+"""
+
+function response_curves end
+
+"""
+    predict(ensemble::SDMensemble, predictor_values::NamedTuple)
+"""
+
+function predict end
+
+
+
+
+
