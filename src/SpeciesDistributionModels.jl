5fff14302432b61469ff4638c9c4501b586a2d55
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-02-04 13:00:34 +0100

    add data utils

--- src/SpeciesDistributionModels.jl
+++ src/SpeciesDistributionModels.jl
@@ -6,22 +6,24 @@ import GLM, PrettyTables, Rasters, EvoTrees, DecisionTree, Makie, Shapley, Loess
 
 using MLJBase: pdf
 
-using Rasters: Raster, RasterStack
+using Rasters: Raster, RasterStack, Band
 
 using Makie: Toggle, Label, scatter!, lines!, Axis, Figure, GridLayout, lift
 
 using ScientificTypesBase: Continuous, OrderedFactor, Multiclass, Count
 
-export SDMensemble, predict, sdm, select, machines, machine_keys, shap, 
+export SDMensemble, predict, sdm_ensemble, select, machines, machine_keys, shap, 
     interactive_evaluation, interactive_response_curves,
     remove_collinear
 
+include("data_utils.jl")
 include("collinearity.jl")
 include("models.jl")
 include("ensemble.jl")
 include("predict.jl")
 include("explain.jl")
 include("evaluate.jl")
+include("interface.jl")
 include("plots.jl")
 
 end
---
+++ src/data_utils.jl
@@ -0,0 +1,14 @@
+### Miscelanious utilities to deal with data issues such as names, missing values
+
+function _get_predictor_names(p, a)
+    predictors = Base.intersect(Tables.schema(a).names, Tables.schema(p).names)
+    predictors = filter!(!=(:geometry), predictors) # geometry is never a variable
+    length(predictors) > 0 || error("Presence and absence data have no common variable names - can't fit the ensemble.")
+    return predictors
+end
+
+function _missingmask(rs::Rasters.AbstractRasterStack)
+    mapreduce(.&, rs) do layer
+        layer .!== Rasters.missingval(layer)
+    end
+end
