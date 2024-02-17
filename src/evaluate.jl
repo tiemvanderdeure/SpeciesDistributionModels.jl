f300cb4a19a726180334d71d3fed55a835bf0d6e
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-02-17 16:10:32 +0100

    cleaner show method for evaluations

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -18,6 +18,7 @@ struct SDMensembleEvaluation <: AbstractVector{SDMgroupEvaluation}
     results
 end
 
+SDMevaluation = Union{SDMmachineEvaluation, SDMgroupEvaluation, SDMensembleEvaluation}
 SDMgroupOrEnsembleEvaluation = Union{SDMgroupEvaluation, SDMensembleEvaluation}
 
 ScoreType = NamedTuple{(:score, :threshold), Tuple{Float64, Union{Missing, Float64}}}
@@ -28,38 +29,43 @@ Base.getindex(group::SDMgroupEvaluation, i) = group.machine_evaluations[i]
 Base.size(ensemble::SDMensembleEvaluation) = Base.size(ensemble.group_evaluations)
 Base.size(group::SDMgroupEvaluation) = Base.size(group.machine_evaluations)
 
-function machine_evaluations(groupeval::SDMgroupEvaluation; mean = false)
+"""
+    machine_evaluations(eval)
+
+    Get the scores for each machine in an evaluation, which can be either an 
+    `SDMgroupEvaluation` or an `SDMensembleEvaluation`.
+
+    The return type is a nested structure of `NamedTuple`s. 
+    The `NamedTuple` returned has two keys `train` and `test`, which each have keys 
+    corresponding to the measures specified in [`evaluate`](@ref).
+
+    ## Example
+    ```julia
+    evaluation = SDM.evaluate(ensemble; measures = (; accuracy, auc))
+    machine_aucs = SDM.machine_evaluations(evaluation).train.auc
+    ```
+
+
+"""
+machine_evaluations
+
+function machine_evaluations(groupeval::SDMgroupEvaluation)
     map((:train, :test)) do set
         map(keys(groupeval.measures)) do key
-            r = map(groupeval) do e
+            map(groupeval) do e
                 e.results[set][key].score
             end
-
-            if mean
-                Statistics.mean(r)
-            else
-                r
-            end
-
         end |> NamedTuple{keys(groupeval.measures)}
     end |> NamedTuple{(:train, :test)}
 end
-
-function machine_evaluations(ensembleeval::SDMensembleEvaluation; mean = false)
+function machine_evaluations(ensembleeval::SDMensembleEvaluation)
     map((:train, :test)) do set
         map(keys(ensembleeval.measures)) do key
-            r = mapreduce(vcat, ensembleeval) do groupeval
+            mapreduce(vcat, ensembleeval) do groupeval
                 map(groupeval) do e
                     e.results[set][key].score
                 end
             end
-
-            if mean
-                Statistics.mean(r)
-            else
-                r
-            end
-
         end |> NamedTuple{keys(ensembleeval.measures)}
     end |> NamedTuple{(:train, :test)}
 end
@@ -77,37 +83,64 @@ function Base.show(io::IO, mime::MIME"text/plain", evaluation::SDMmachineEvaluat
     PrettyTables.pretty_table(io, table_cols; header = header)
 end
 
-function Base.show(io::IO, mime::MIME"text/plain", evaluation::SDMgroupOrEnsembleEvaluation)
+function Base.show(io::IO, mime::MIME"text/plain", evaluation::SDMgroupEvaluation)
     measures = collect(keys(evaluation.measures))
-    train_scores, test_scores = machine_evaluations(evaluation, mean = true)
-    
-    group_scores = map(measures) do key
-        evaluation.results[key].score
-    end
+    train_scores, test_scores = machine_evaluations(evaluation)
+    folds = getfield.(evaluation.group, :fold)
 
     println(io, "$(typeof(evaluation)) with $(length(measures)) performance measures")
 
-    table_cols = hcat(measures, collect(group_scores), collect(train_scores), collect(test_scores))
-    header = (["measure", "performance of avg", "avg. train performance", "avg. test performance"])
-    PrettyTables.pretty_table(io, table_cols; header = header)
+    println(io, "Testing data")
+    PrettyTables.pretty_table(io, merge((; fold = folds),  test_scores))
+    println(io, "Training data")
+    PrettyTables.pretty_table(io, merge((; fold = folds),  train_scores))
+end
+
+function Base.show(io::IO, mime::MIME"text/plain", evaluation::SDMensembleEvaluation)
+    measures = collect(keys(evaluation.measures))
+    models = getfield.(evaluation.ensemble, :model_name)
+
+    # get scores from each group
+    scores = machine_evaluations.(evaluation)
+    # get mean test and train from each group for each measure.
+    # then invert to a namedtuple where measures are keys
+    test_scores = map(scores) do score
+        map(Statistics.mean, score.test)
+    end |> Tables.columntable
+    train_scores = map(scores) do score
+        map(Statistics.mean, score.train)
+    end |> Tables.columntable
     
+    println(io, "$(typeof(evaluation)) with $(length(measures)) performance measures")
+
+    println(io, "Testing data")
+    PrettyTables.pretty_table(io, merge((; model = models),  test_scores))
+    println(io, "Training data")
+    PrettyTables.pretty_table(io, merge((; model = models),  train_scores))
 end
 
 ## Core evaluator
 # internal method to get a vector of scores from y_hats, ys, and a namedtuple of measures
 function _evaluate(y_hat::MLJBase.UnivariateFiniteArray, y::CategoricalArrays.CategoricalArray, measures)
-    map(measures) do measure
+    kinds_of_proxy = map(StatisticalMeasuresBase.kind_of_proxy, measures)
+    
+    # if any are literal targets (threshold-dependent), compute the confusion matrices outside the loop
+    if any(map(kind -> kind == StatisticalMeasures.LearnAPI.LiteralTarget(), kinds_of_proxy))
+        scores = pdf.(y_hat, true)
+        thresholds = unique(scores)
+        thresholded_scores = map(t -> CategoricalArrays.categorical(scores .>= t, levels = [false, true]), thresholds)
+        conf_mats = StatisticalMeasures.ConfusionMatrix(; levels = [false, true], checks = false).(thresholded_scores, Ref(y))
+    else
+        conf_mats = nothing
+    end
+
+    map(measures, kinds_of_proxy) do measure, kind
         # If the measures is threshold independent
-        if StatisticalMeasuresBase.kind_of_proxy(measure) == StatisticalMeasures.LearnAPI.Distribution()
+        if kind == StatisticalMeasures.LearnAPI.Distribution()
             return ScoreType((score = measure(y_hat, y), threshold = missing))
-        else # else the measure uses thresholds
-            # first get all possible thresholded values
-            scores = pdf.(y_hat, true)
-            thresholds = unique(scores)
-            thresholded_scores = map(t -> CategoricalArrays.categorical(scores .>= t, levels = [false, true]), thresholds)
-            
+        else # else the measure uses thresholds    
             # find the max value and corresponding threshold for measure
-            all_scores = measure.(thresholded_scores, Ref(y))
+            all_scores = measure.(conf_mats)
             max_score = findmax(all_scores)
             return ScoreType((score = max_score[1], threshold = thresholds[max_score[2]]))
         end
