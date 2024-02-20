a104ce3992ecc84e69f907d87be9ce0179496086
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-02-20 12:31:16 +0100

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,53 +1,62 @@
-using SpeciesDistributionModels, CategoricalArrays
+using SpeciesDistributionModels, MLJBase
 import SpeciesDistributionModels as SDM
+using StableRNGs, Distributions, Test
 
-import GLM: Distributions
-
+rng = StableRNG(0)
 # some mock data
 n = 500
-backgrounddata = (a = rand(n), b = rand(n), c = categorical(rand(0:3, n)))
-presencedata = (a = rand(n), b = rand(n).^2, c = categorical(rand(Distributions.Binomial(3, 0.5), n)))
-
-using Test
+backgrounddata = (a = rand(rng, n), b = rand(rng, n), c = rand(rng, n))
+presencedata = (a = rand(rng, n), b = rand(rng, n).^2, c = sqrt.(rand(rng, n)))
 
 @testset "SpeciesDistributionModels.jl" begin
-    models = [SDM.random_forest(), SDM.random_forest(; max_depth = 3), SDM.linear_model(), SDM.boosted_regression_tree()]
+    models = [SDM.random_forest(; rng), SDM.random_forest(; max_depth = 3, rng), SDM.linear_model(), SDM.boosted_regression_tree()]
 
-    ensemble = sdm_ensemble(
+    ensemble = sdm(
         presencedata, backgrounddata;
-        models = models, resampler = SDM.MLJBase.CV(; shuffle = true, nfolds = 5)
+        models = models, resampler = SDM.MLJBase.CV(; shuffle = true, nfolds = 5, rng), threaded = false
     )
 
     evaluation = SDM.evaluate(ensemble)
-
-    pr1 = SDM.predict(ensemble, backgrounddata)
-    pr2 = SDM.predict(ensemble, backgrounddata; reducer = maximum)
-    pr3 = SDM.predict(ensemble, backgrounddata; reducer = maximum, by_group = true)
-
-    # Need much better tests
     @test evaluation isa SDM.SDMensembleEvaluation
     @test evaluation[1] isa SDM.SDMgroupEvaluation
     @test evaluation[1][1] isa SDM.SDMmachineEvaluation
-    
+    @test evaluation.measures isa NamedTuple
+    mach_evals = SDM.machine_evaluations(evaluation)
+    @test mach_evals isa NamedTuple{(:train, :test)}
+    @test mach_evals.train isa NamedTuple{(keys(evaluation.measures))}
+
+    machine_aucs = SDM.machine_evaluations(evaluation).test.auc
+
+    pr1 = SDM.predict(ensemble, backgrounddata)
+    pr2 = SDM.predict(ensemble, backgrounddata; reducer = maximum)
+    pr3 = SDM.predict(ensemble, backgrounddata; reducer = x -> sum(x .> 0.5), by_group = true)
+
     @test pr2 isa Vector
     @test collect(keys(pr1)) == SDM.machine_keys(ensemble)
     @test collect(keys(pr3)) == SDM.model_names(ensemble)
+    eltype(pr3) == Vector{Int64}
 
     @test_throws ArgumentError SDM.predict(ensemble, backgrounddata.a)
     @test_throws Exception SDM.predict(ensemble, backgrounddata[(:a,)])
     @test_throws Exception SDM.predict(ensemble, backgrounddata; by_group = true)
+
+    # explain
+    expl = explain(ensemble; method = ShapleyValues(10; rng))
+    varimp = variable_importance(expl)
+    @test varimp.b > varimp.a
+    @test varimp.c > varimp.a
 end
 
 @testset "collinearity" begin
     # mock data with a collinearity problem
-    data_with_collinearity = merge(backgrounddata, (; d = backgrounddata.a .+ rand(n), e = backgrounddata.a .+ rand(n), f = f = categorical(rand(Distributions.Binomial(3, 0.5), 500))    ))
+    data_with_collinearity = merge(backgrounddata, (; d = backgrounddata.a .+ rand(rng, n), e = backgrounddata.a .+ rand(rng, n), f = f = categorical(rand(Distributions.Binomial(3, 0.5), 500))    ))
 
-    rm_col_gvif = remove_collinear(data_with_collinearity; method = SDM.Gvif(; threshold = 2.), silent = true)
+    rm_col_gvif = remove_collinear(data_with_collinearity; method = SDM.Gvif(; threshold = 2.), silent = false)
     rm_col_vif = remove_collinear(data_with_collinearity; method = SDM.Vif(; threshold = 2.), silent = true)
     rm_col_pearson = remove_collinear(data_with_collinearity; method = SDM.Pearson(; threshold = 0.65), silent = true)
     @test rm_col_gvif == (:b, :c, :d, :e, :f)
-    @test rm_col_vif == (:b, :d, :e, :c, :f)
-    @test rm_col_pearson == (:b, :d, :e, :c, :f)
+    @test rm_col_vif == (:b, :c, :d, :e, :f)
+    @test rm_col_pearson == (:b, :c, :d, :e, :f)
 
     data_with_perfect_collinearity = (a = [1,2,3], b = [1,2,3])
     Test.@test_throws Exception remove_collinear(data_with_perfect_collinearity; method = SDM.Gvif(; threshold = 2., remove_perfectly_collinear = false), silent = true)
