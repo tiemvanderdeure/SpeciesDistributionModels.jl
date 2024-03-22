5a385191d99081ca98f26d4101b02c5e6cfcf74d
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-03-22 10:18:12 +0100

    add noresampling resampling strategy

--- src/SpeciesDistributionModels.jl
+++ src/SpeciesDistributionModels.jl
@@ -11,6 +11,7 @@ using ComputationalResources: CPU1, CPUThreads, AbstractCPU
 
 using ScientificTypesBase: Continuous, OrderedFactor, Multiclass, Count
 using StatisticalMeasures: auc, kappa, sensitivity, selectivity, accuracy
+import MLJBase: StratifiedCV, CV, Holdout, ResamplingStrategy
 
 export SDMensemble, predict, sdm, select, machines, machine_keys,
     remove_collinear,
@@ -19,9 +20,12 @@ export SDMensemble, predict, sdm, select, machines, machine_keys,
     SDMmachineEvaluation, SDMgroupEvaluation, SDMensembleEvaluation
 
 # re-export
-export auc, kappa, sensitivity, selectivity, accuracy
+export auc, kappa, sensitivity, selectivity, accuracy,
+    Continuous, OrderedFactor, Multiclass, Count,
+    StratifiedCV, CV, Holdout, ResamplingStrategy
 
 include("data_utils.jl")
+include("resample.jl")
 # export stubs for extensions
 export interactive_response_curves, interactive_evaluation
 
---
+++ src/resample.jl
@@ -0,0 +1,4 @@
+struct NoResampling <: MLJBase.ResamplingStrategy end
+MLJBase.train_test_pairs(::NoResampling, indices, _)  = [(indices, eltype(indices)[])]## get indices
+
+struct CustomRows end
