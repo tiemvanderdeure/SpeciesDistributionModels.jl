89f0114638f38bc04db4d2c2c3bf127343c7ce23
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-11-22 15:20:48 +0100

    use from MLJBase once

--- src/SpeciesDistributionModels.jl
+++ src/SpeciesDistributionModels.jl
@@ -5,12 +5,11 @@ import MLJBase, StatisticalMeasures, CategoricalArrays
 import GLM, PrettyTables, Rasters, Shapley, Loess, Distances
 import GeoInterface as GI
 
-using MLJBase: pdf
 using Rasters: Raster, RasterStack, Band, DD
 using ComputationalResources: CPU1, CPUThreads, AbstractCPU, CPUProcesses
 using StatisticalMeasures: auc, kappa, sensitivity, selectivity, accuracy, StatisticalMeasuresBase
 using ScientificTypesBase: Continuous, OrderedFactor, Multiclass, Count
-import MLJBase: StratifiedCV, CV, Holdout, ResamplingStrategy, Machine, Probabilistic
+import MLJBase: StratifiedCV, CV, Holdout, ResamplingStrategy, Machine, Probabilistic, pdf
 
 export SDMensemble, predict, sdm, sdmdata, select, machines, machine_keys,
     remove_collinear, thin,
