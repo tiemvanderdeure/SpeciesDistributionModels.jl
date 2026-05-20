b721ccf83341b9421b248c3b9a5f5bbf91b3eb89
tiemvanderdeure <tiemvanderdeure@gmail.com>
2026-05-20 17:41:34 +0200

    abstractdimarray not dimarray

--- src/predict.jl
+++ src/predict.jl
@@ -42,7 +42,7 @@ function _predict!(output::RA.AbstractRaster, data::RA.AbstractRasterStack, e)
     @views _predict!(output[bm], data[bm], e)
     return output
 end
-function _predict!(output::DimArray, data, e::SDMensemble, threaded::Bool)
+function _predict!(output::DD.AbstractDimArray, data, e::SDMensemble, threaded::Bool)
     @maybe_threads threaded for I in DD.DimIndices(e)
         _predict!(view(output, I), data, e[I])
     end
@@ -54,4 +54,4 @@ function _predict!(output::AbstractArray, data, m)
     return output
 end
 # view(output, I) can result in a 0-dimension array
-_predict!(output::DimArray{<:AbstractArray, 0}, data, m) = _predict!(first(output), data, m)
+_predict!(output::DD.AbstractDimArray{<:AbstractArray, 0}, data, m) = _predict!(first(output), data, m)
